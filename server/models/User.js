1f4fd038f463cbc11ca08a047cdbc8a039ded212
Vansh Sachdeva <vanshsachdeva900@gmail.com>
2025-07-27 12:02:21 +0530

    updated5

--- server/models/User.js
+++ server/models/User.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String },
+  password: { type: String, required: true },
   role: { type: String, enum: ['user', 'therapist', 'admin'], default: 'user', required: true },
   googleId: { type: String },
   specialization: {
